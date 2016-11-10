0a885bb4dc466ece50e1badb75e2a7ff33f89a8c
유명식 <MyeongSic@MyeongSic.local>
2016-11-10 18:56:56 +0900

    Incoming Servcie State Update

--- app/src/main/java/com/example/myeongsic/androidcodestore/incoming/IncomingCallBroadcastReceiver.java
+++ app/src/main/java/com/example/myeongsic/androidcodestore/incoming/IncomingCallBroadcastReceiver.java
@@ -17,19 +17,41 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "onRceive()");
-
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-        if (state.equals(mLastState)) {//에러부분,중복작동방지
-            return;
-        } else { //불러오는부분
-            mLastState = state;
-            if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
-                Intent serviceIntent = new Intent(context, CallingService.class);
-                context.startService(serviceIntent);
-            }
+        if(state.equals(TelephonyManager.EXTRA_STATE_IDLE))
+        {
+            Log.d(TAG,"통화 종료 후 구현 ...");
         }
+        else if(state.equals(TelephonyManager.EXTRA_STATE_RINGING))
+        {
+
+            Log.d(TAG,"통화 벨 울릴 시 구현 ...");
+
+            if (state.equals(mLastState)) {//에러부분,중복작동방지
+                return;
+            } else { //불러오는부분
+                mLastState = state;
+                if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
+                    Intent serviceIntent = new Intent(context, CallingService.class);
+                    context.startService(serviceIntent);
+                }
+            }
+
+        }
+        else if(state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))
+        {
+            Log.d(TAG,"통화 중 상태일 때 구현 ...");
+
+        }
+        else if(intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL))
+        {
+
+            Log.d(TAG,"전화를 걸때 상태 구현 ...");
+        }
+
+
+
     }
 
 
